508106534a0b4e89144c3687f4f864a558e0ac67
khanhtv28 <khanhtv28@fpt.com>
2024-11-12 14:36:51 +0700

    .

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/run-ovn.sh
 curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/run-ovs.sh -o run-ovs.sh
 curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/export.sh -o export.sh
 curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/alias-ovn.sh -o alias-ovn.sh
+curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/add-phys.sh -o add-phys.sh
 
 chmod u+x *.sh 
 
