58f1b7be8457dda71a6cc6aba44175989ef3265c
khanhtv28 <khanhtv28@fpt.com>
2024-11-12 14:44:49 +0700

    .

--- add-phys.sh
+++ add-phys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 BR=$1
 PHYS_INT=$2
 PHYS_NAME=$3 
@@ -7,4 +9,11 @@ PHYS_NAME=$3
 ovs-vsctl add-br $1
 ovs-vsctl add-port $1 $2
 
-ovs-vsctl set open . external-ids:ovn-bridge-mappings=$3:$1
+CURRENT=$(ovs-vsctl get open . external-ids:ovn-bridge-mappings)
+
+if [ $? -ne 0]; then 
+    ovs-vsctl set open . external-ids:ovn-bridge-mappings=$3:$1
+    exit 0
+fi
+
+ovs-vsctl set open . external-ids:ovn-bridge-mappings=$CURRENT,$3:$1
