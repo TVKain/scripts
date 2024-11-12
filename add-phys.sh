f4ad403320ec851f30e33b0dcbacf3e33c41c043
khanhtv28 <khanhtv28@fpt.com>
2024-11-12 15:03:09 +0700

    .

--- add-phys.sh
+++ add-phys.sh
@@ -9,11 +9,15 @@ PHYS_NAME=$3
 ovs-vsctl add-br $1
 ovs-vsctl add-port $1 $2
 
-CURRENT=$(ovs-vsctl get open . external-ids:ovn-bridge-mappings)
 
-if [ $? -ne 0]; then 
+CURRENT=$(ovs-vsctl get open . external-ids:ovn-bridge-mappings | cut -d'"' -f 2)·
+
+ERR=$?
+
+if [ "$ERR" -ne "0" ]; then·
     ovs-vsctl set open . external-ids:ovn-bridge-mappings=$3:$1
     exit 0
 fi
+
 
 ovs-vsctl set open . external-ids:ovn-bridge-mappings=$CURRENT,$3:$1
