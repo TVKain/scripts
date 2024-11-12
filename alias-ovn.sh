ce98e02270c2b19aced6f74164e9a1f23090fd72
khanhtv28 <khanhtv28@fpt.com>
2024-11-12 11:51:38 +0700

    .

--- alias-ovn.sh
+++ alias-ovn.sh
@@ -2,5 +2,5 @@
 
 DB=$1
 
-alias ovn-nbctl=ovn-nbctl --db tcp:$DB:6641
-alias ovn-sbctl=ovn-sbctl --db tcp:$DB:6642
+alias ovn-nbctl="ovn-nbctl --db tcp:$DB:6641"
+alias ovn-sbctl="ovn-sbctl --db tcp:$DB:6642"
