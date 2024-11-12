1b6dbd851e494bdcdf107e1501bc480c330230ce
khanhtv28 <khanhtv28@fpt.com>
2024-11-12 11:12:24 +0700

    add: setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/install-ovn.sh
+curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/configure-node.sh
+curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/run-ovn.sh
+curl https://raw.githubusercontent.com/TVKain/scripts/refs/heads/main/run-ovs.sh
